3b430357cac15c257438e3984573ac30bb46913e
Fabian Abdallah <fabian.abdallah98@googlemail.com>
2021-02-12 23:04:45 +0100

    changes

--- Transformation/transformation.js
+++ Transformation/transformation.js
@@ -6,51 +6,80 @@ const fs = require("fs");
 const graphql = require("graphql");
 const OtG = require("openapi-to-graphql");
 
-function Transformation() {
-  this.createGraphQLWrapper = async (document) => {
-  
-    const oas = require("./Openapi.json");
-    const { schema } = await OtG.createGraphQLSchema(oas);
- 
+function Transformation(document) {
+  this.document = document;
+  //document is path to document
+  this.createGraphQLWrapper = async () => {
+    const codeForWrapper = `use strict";
 
-    return graphql.printSchema(schema);
-  };
+    const util = require("util");
+    
+    const express = require("express");
+    const { graphqlHTTP } = require("express-graphql");
+    const OtG = require("openapi-to-graphql");
+    
+    async function startServer() {
+      // use OpenAPI-to-GraphQL to create a GraphQL schema:
+      const oas = require(${this.document});
+      const { schema } = await OtG.createGraphQLSchema(
+        oas,${this.resolverTemplate()}
+      );
+      const app = express();
+    
+      app.use(express.json());
+      app.use(express.urlencoded());
+      // MIDDLEWARE SOFTWARE SCHREIBEN DIE ALLE ANFRAGEN VON DER NORMALEN API ZU MEINER GRAPHQL API WEITERLEITET
+    
+      //app.use();
+      app.use(
+        "/",
+        graphqlHTTP({
+          schema: schema,
+          graphiql: true,
+        })
+      );
+    
+      app.listen(3001);
+    }
+    
+    // Kick things off:
+    startServer();
+    `;
 
-  this.createGraphQLOperations = () => {
-    var schema = `schema {
-      query: Query,
-      mutation: Mutation
-    }`;
-    return "";
+    return codeForWrapper;
   };
 
   this.resolverTemplate = () => {
-    return "";
+    var customResolver = `{customResolver:{ "${
+      oas.info.title
+    }":{${this.fillResolver()}},},}`;
+    return customResolver;
   };
 
-  this.fillResolver = ()=>  {
-    return "";
+  this.fillResolver = () => {
+    var result = "";
+    var objectPaths = Object.keys(oas.paths);
+    for (var path in objectPaths) {
+      var methodsPerPath = Object.keys(oas.paths[objectPaths[path]]);
+      //console.log(objectPaths[path]);
+      var methodsPerPathStringArray = methodsPerPath.map((method) => {
+        return `${method}: (obj, args, context, info) => {
+            return null;
+          },`;
+      });
+      //console.log(methodsPerPathStringArray);
+      var onePath = `"${objectPaths[path]}":{`;
+
+      methodsPerPathStringArray.forEach((method) => {
+        onePath += method;
+      });
+
+      onePath += "},";
+      result += onePath;
+    }
+
+    return result;
   };
-
-  this.createGraphQLServer = () => {
-    var imports =
-      "var express = require('express');\nvar { graphqlHTTP } = require('express-graphql');\nvar { buildSchema } = require('graphql');";
-    var schema = `var schema = buildSchema(${this.createGraphQLSchema()}\n${this.createGraphQLOperations()});`;
-
-    var root = `var root = {${this.createResolver()}};`;
-
-    // hier müssen noch die ganzen url der Restclients reinkommen damit sie nichts von den veränderungen bemerken
-    var rest = `var app = express();
-    app.use('/graphql', graphqlHTTP({
-      schema: schema,
-      rootValue: root,
-      graphiql: true,
-    }));
-    app.listen(4000);
-    console.log('Running a GraphQL API server at localhost:4000/graphql');`;
-    return imports + "\n" + schema + "\n" + root + "\n" + rest;
-  };
-
   //makes a script and runs the script so the graphqlserver runs
   this.runServer = () => {
     // this.createGraphQLSchema(inputData.entpoints);
